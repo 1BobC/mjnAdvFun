060d0d05e2a4fdeb587db3238e878d66f0aa9131
Bob.C <bob.cocksedge@gmail.com>
2021-04-09 11:39:01 +0100

    Completed nested functions Set up files for Explanation of functions and Exercises

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # mjnAdvFun
 Modern Javascript exercises for variable scope to closure Especially nested functions
 "Set up files for exercises Advanced working with functions"
+"Completed code blocks to error without blocks Ready to start if for while variables"
+"Completed nested functions Set up files for Explanation of functions and Exercises"
+
 
 
--- jscript.js
+++ jscript.js
@@ -23,11 +23,97 @@
 // }
 
 //Without code blocks
-"use strict"
-    let message= "Hello";
-    console.log(message);
-    document.getElementById("bob4").innerHTML= "Using the first variable name, message is: " + message;
+// "use strict"
+//     let message= "Hello";
+//     console.log(message);
+//     document.getElementById("bob4").innerHTML= "Using the first variable name, message is: " + message;
 
-    let message= "Goodbye";
-    console.log(message);
-    document.getElementById("bob5").innerHTML= "Using the same variable name, message is: " + message;
+//     let message= "Goodbye";
+//     console.log(message);
+//     document.getElementById("bob5").innerHTML= "Using the same variable name, message is: " + message;
+
+//if for while and so on, variables declared in {...} are also only visible inside:
+//     if (true){"use strict"
+//         let phrase= "Hello again";
+//         console.log(phrase)
+//         document.getElementById("bob6").innerHTML= "if for while and so on, variables declared in {...} are also only visible inside: " + phrase;
+// }
+//         console.log(phrase)
+//         document.getElementById("bob6").innerHTML= "if for while and so on, variables declared in {...} are also only visible inside: " + phrase; 
+        
+//for and while loops, variables declared in {...} are also only visible inside:
+    // for (i= 0; i< 3; i++){
+    //     alert(i);
+    // }
+    //     alert(i);
+
+//Nested functions. A function is called “nested” when it is created inside another function!!
+// let firstName= "roy";
+// let lastName= "rogers";
+        //function sayHiBi(firstName, lastName) {
+        // let firstName= "roy";
+        // let lastName= "rogers";
+        //function getFullName(){
+        // let firstName= "roy";                    //only when variables declared here does it work - why?
+        // let lastName= "rogers";
+    //         return firstName + " " + lastName;
+    //     }
+    //         document.getElementById("bob8").innerHTML= ("Hello and Goodbye " + getFullName());
+    //         alert("Hello, " + getFullName());
+    //         alert("Bye, " + getFullName());
+    // }
+    //          sayHiBi();
+
+//More Nested -  a nested function can be returned: either as a property of a new object 
+//or as a result by itself. It can then be used somewhere else. No matter where, 
+//it still has access to the same outer variables.
+    // function makeCounter(){ "use strict"
+    //     let count= 0;
+    //     return function(){
+    //         return count++;
+    //     };   
+    // }
+    //     let counter= makeCounter();
+    //     alert (counter());
+    //     alert (counter());
+    //     alert (counter());
+    //     alert (counter());
+    //     alert (counter());
+    //     alert (counter());
+    //     alert (counter());
+    //     alert (counter());
+    //     alert (counter());
+
+//Now see explanation!
+//Step 1. Variables
+//Step 2. Function Declarations
+//Step 3. Inner and outer Lexical Environment
+    let phrase= "Hello";
+    function say(name){ "use strict"
+        //alert(`${phrase}, ${name}`);
+        return (`${phrase}, ${name}`);
+    }
+        //say("John");
+         document.getElementById("bob9").innerHTML= "The phrase is: " + say("John");
+         
+//Step 4. Returning a function
+//Info - Closure
+//Info - Garbage collection
+//Info - Real-life optimizations
+//Exercises - Tasks - incudes solutions!
+//1 - Does a function pickup latest changes?
+//2 - Which variables are available?
+//3 - Are counters independent?
+//4 - Counter object
+//5 - Function in if
+//6 - Sum with closures
+//7 - Is variable visible?
+//8 - Filter through function
+//9 - Sort by field
+//10 - Army of functions
+
+
+
+
+
+
